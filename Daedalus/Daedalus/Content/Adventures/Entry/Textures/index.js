f6acdee17918ac545028debe6840dd09a3b49860
Ben Goltz <ben.goltz@outlook.com>
2016-07-31 22:23:47 -0500

    Reorganized Creature textures.

--- Daedalus/Daedalus/Content/Adventures/Entry/Textures/index.js
+++ Daedalus/Daedalus/Content/Adventures/Entry/Textures/index.js
@@ -1,15 +1,15 @@
 // Females
-exports.femaleHealer = Content.LoadTexture2D(__dirname + "/Female/Healer.png");
-exports.femaleMage = Content.LoadTexture2D(__dirname + "/Female/Mage.png");
-exports.femaleNinja = Content.LoadTexture2D(__dirname + "/Female/Ninja.png");
-exports.femaleRanger = Content.LoadTexture2D(__dirname + "/Female/Ranger.png");
-exports.femaleTownfolk = Content.LoadTexture2D(__dirname + "/Female/Townfolk.png");
-exports.femaleWarrior = Content.LoadTexture2D(__dirname + "/Female/Warrior.png");
+exports.femaleHealer = Content.LoadTexture2D(__dirname + "/Creatures/Female/Healer.png");
+exports.femaleMage = Content.LoadTexture2D(__dirname + "/Creatures/Female/Mage.png");
+exports.femaleNinja = Content.LoadTexture2D(__dirname + "/Creatures/Female/Ninja.png");
+exports.femaleRanger = Content.LoadTexture2D(__dirname + "/Creatures/Female/Ranger.png");
+exports.femaleTownfolk = Content.LoadTexture2D(__dirname + "/Creatures/Female/Townfolk.png");
+exports.femaleWarrior = Content.LoadTexture2D(__dirname + "/Creatures/Female/Warrior.png");
 
 // Males
-exports.maleHealer = Content.LoadTexture2D(__dirname + "/Male/Healer.png");
-exports.maleMage = Content.LoadTexture2D(__dirname + "/Male/Mage.png");
-exports.maleNinja = Content.LoadTexture2D(__dirname + "/Male/Ninja.png");
-exports.maleRanger = Content.LoadTexture2D(__dirname + "/Male/Ranger.png");
-exports.maleTownfolk = Content.LoadTexture2D(__dirname + "/Male/Townfolk.png");
-exports.maleWarrior = Content.LoadTexture2D(__dirname + "/Male/Warrior.png");
+exports.maleHealer = Content.LoadTexture2D(__dirname + "/Creatures/Male/Healer.png");
+exports.maleMage = Content.LoadTexture2D(__dirname + "/Creatures/Male/Mage.png");
+exports.maleNinja = Content.LoadTexture2D(__dirname + "/Creatures/Male/Ninja.png");
+exports.maleRanger = Content.LoadTexture2D(__dirname + "/Creatures/Male/Ranger.png");
+exports.maleTownfolk = Content.LoadTexture2D(__dirname + "/Creatures/Male/Townfolk.png");
+exports.maleWarrior = Content.LoadTexture2D(__dirname + "/Creatures/Male/Warrior.png");
